3d975717e91615f23b20ea24aa7a6b9233464492
李子航 <2045268468@qq.com>
2025-04-30 22:33:52 +0800

    2025-4-30/2

--- react/src/event/long_press/index.tsx
+++ react/src/event/long_press/index.tsx
@@ -1,8 +1,7 @@
 import { useLongPress } from './useLongPressV2';
 
 const LongPress = () => {
-
-    const longPressProps = useLongPress({
+  const longPressProps = useLongPress({
     onLongPressStart: (e) => console.log('长按开始!', e.type),
     onLongPressEnd: (e) => console.log('长按结束!', e.type),
     onClick: (e) => console.log('点击!', e.type),
@@ -12,10 +11,7 @@ const LongPress = () => {
 
   return (
     <div className="h-screen w-screen flex justify-center items-center">
-      <div
-        className="w-40 h-40 bg-blue-400"
-        {...longPressProps}
-      ></div>
+      <div className="w-40 h-40 bg-blue-400" {...longPressProps}></div>
     </div>
   );
 };
